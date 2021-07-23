cee2b63a16afa8ae0fe9f5082fd6a86e37f43904
gabrielnndoan <gabrielnndoan@gmail.com>
2021-07-23 13:47:17 -0500

    made routes for home, history, and search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import {History, Home, Search} from "./components"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/history">HISTORY</Link>
+        <Link to="/search">SEARCH</Link>
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/history">
+            <History/>
+          </Route>
+          <Route path="/search">
+            <Search/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as History} from "./History"
+export { default as Home} from "./Home"
+export { default as Search} from "./Search"
